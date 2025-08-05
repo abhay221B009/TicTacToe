c5147d5860c320c9be623a67e7dc70204f0c2d55
Abhay'sRepo <abhayrajchauhan.976@gmail.com>
2025-08-06 03:53:29 +0530

    created a TicTacToe game

---
+++ app.js
@@ -0,0 +1,72 @@
+let boxes = document.querySelectorAll(".box");
+let resetButton = document.querySelector(".reset");
+let newGaameBtn = document.querySelector(".new-btn"); // fixed class name
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+let turnO = true; //player x
+
+//2d-array
+const winPatterns = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+const resetGame = () => {
+  turnO = true;
+  enableBoxes();
+  msgContainer.classList.add("hide");
+};
+
+const disableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
+const enableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+};
+
+const showWinner = (winner) => {
+  msg.innerText = `Congratulations! ${winner} is the winner!`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    let pos1 = boxes[pattern[0]].innerHTML;
+    let pos2 = boxes[pattern[1]].innerHTML;
+    let pos3 = boxes[pattern[2]].innerHTML;
+    if (pos1 != "" && pos2 != "" && pos3 != "") {
+      if (pos1 === pos2 && pos2 === pos3) {
+        showWinner(pos1);
+      }
+    }
+  }
+};
+
+newGaameBtn.addEventListener("click", resetGame);
+resetButton.addEventListener("click", resetGame);
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turnO) {
+      box.innerHTML = "O";
+      turnO = false;
+    } else {
+      box.innerHTML = "X";
+      turnO = true;
+    }
+    box.disabled = true;
+    checkWinner();
+  });
+});
